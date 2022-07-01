8314e374a67bf1aea7436a9bef2ec4f3851084f6
patrice mocombe <patricemocombe@gmail.com>
2022-07-01 16:30:20 -0400

    commit tree 438ab988907ea22209235f610dd8a761161fa1b2

--- index.js
+++ index.js
@@ -1,4 +1,19 @@
-let countdown = 15;
-while (countdown > -1) {
+function writeCards(friends, holiday){
+    let thankYouCards = []
+    for (let i = 0; i < friends.length; i++){
+        thankYouCards.push(`Thank you, ${friends[i]}, for the wonderful ${holiday} gift!`)
+       
+    }
+
+
+    return thankYouCards
+};
+writeCards(["Lisa", "Kaitlin", "Jan"], "surprise");
+
+function countDown (countdown) {
+    while (countdown > 0) {
     console.log(countdown--);
+};
+    console.log(countdown);
 }
+countDown(15);
